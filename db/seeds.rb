7b2f169ca04108798c545d30dd9d7849d45b3187
Sam Lin <greenlin111@gmail.com>
2017-12-19 01:19:47 +0800

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+["Action", "Adventure", "Fantasy", "Animation", "Science Fiction", "Drama", "Thriller", "Family", "Comedy"].each do |name|
+  Genre.create(name: name)
+  puts "分類： #{name} 新增完成"
+end
+
+data = CSV.read(File.join(File.dirname(__dir__), 'tmp', 'movies_stats.csv'))
+
+data.each do |row|
+  movie = Movie.new
+  movie.genre_id = Genre.find_by(name: row.shift).id
+  movie.language = row.shift
+  movie.title = row.shift
+  movie.overview = row.shift
+  movie.release_date = Date.parse(row.shift)
+  movie.revenue = row.shift.to_i
+  movie.runtime = row.shift.to_i
+  movie.save
+  puts "電影：#{movie.title} 新增完成"
+end
